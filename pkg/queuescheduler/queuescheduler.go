d9d56e6cfd13b0532d6e4526db10da2e57fd9c03
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2022-02-15 10:08:43 +0530

    fix timestamp priority issue

--- pkg/queuescheduler/queuescheduler.go
+++ pkg/queuescheduler/queuescheduler.go
@@ -37,24 +37,31 @@ func (qs QueueScheduler) Name() string {
 }
 
 // Check if the pod has a label called priority and if it does, then give this pod preference
-func GetPodPriority(podInfo *framework.QueuedPodInfo, currentUnixTime int64) int32 {
+func GetPodPriority(podInfo *framework.QueuedPodInfo, currentUnixTime int64) (int32, bool) {
 	pod := podInfo.Pod
 
 	if pod.ObjectMeta.Labels == nil {
-		return int32(currentUnixTime - podInfo.Timestamp.Unix())
+		return int32(currentUnixTime - podInfo.Timestamp.Unix()), false
 	}
 
-	// fmt.Println("pod.ObjectMeta.Labels", pod.ObjectMeta.Labels)
+	// 1. check if label "priority" exists
+	// 2. check if the value of that is of the format - priority-number
 	if labelValue, ok := pod.ObjectMeta.Labels["priority"]; ok {
-		priority, err := strconv.Atoi(strings.Split(labelValue, "-")[1])
+		if len(labelValue) > 0 {
+			prioritySplit := strings.Split(labelValue, "-")
 
-		if err == nil {
-			return int32(priority)
+			if prioritySplit[0] == "priority" {
+				priority, err := strconv.Atoi(prioritySplit[1])
+
+				if err == nil {
+					return int32(priority), true
+				}
+			}
 		}
 	}
 
 	// if one pod was scheduled earlier than give that pod higer priority
-	return int32(currentUnixTime - podInfo.Timestamp.Unix())
+	return int32(currentUnixTime - podInfo.Timestamp.Unix()), false
 }
 
 // Less is the function used by the activeQ heap algorithm to sort pods.
@@ -63,8 +70,18 @@ func GetPodPriority(podInfo *framework.QueuedPodInfo, currentUnixTime int64) int
 func (qs *QueueScheduler) Less(pInfo1, pInfo2 *framework.QueuedPodInfo) bool {
 	currentUnixTime := time.Now().Unix()
 
-	priority1 := GetPodPriority(pInfo1, currentUnixTime)
-	priority2 := GetPodPriority(pInfo2, currentUnixTime)
+	priority1, labelExists1 := GetPodPriority(pInfo1, currentUnixTime)
+	priority2, labelExists2 := GetPodPriority(pInfo2, currentUnixTime)
+
+	// if label exists for either of the two pods, then just use that as priority
+	// else use the Timestamp difference
+	if labelExists1 && labelExists2 {
+		return priority1 > priority2
+	} else if labelExists1 && !labelExists2 {
+		return true
+	} else if !labelExists1 && labelExists2 {
+		return false
+	}
 
 	log(fmt.Sprintf("Priority1 = %d, Priority2 = %d\n", priority1, priority2), "info")
 
